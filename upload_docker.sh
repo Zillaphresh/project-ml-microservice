b0c184e626edfd200c99429db2bcd01465194e9f
EC2 Default User <ec2-user@ip-172-31-42-73.ec2.internal>
2022-08-26 15:57:22 +0000

    adding upload docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="zillaphresh/ml_microservice_kubernetes"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag ml_microservice_kubernetes $dockerpath
+docker login --username zillaphresh
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
