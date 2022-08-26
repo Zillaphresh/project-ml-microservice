52884011b3da8932f9450c2df4895c500e4ae5e0
Godwin Agbenyenu <ec2-user@ip-172-31-33-44.ec2.internal>
2022-08-26 15:34:08 +0000

    adding shell files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="zillaphresh/ml_microservice_kubernetes"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run ml-microservice-kubernetes --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-microservice-kubernetes 8000:80
